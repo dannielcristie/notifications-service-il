d72aeb6ca97fa940b51e5dfcdea48a4cd02648ac
Danniel Cristie <dany14012@gmail.com>
2022-12-19 11:26:43 -0300

    chore: moving

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,17 @@
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
+import { NotificationRepository } from 'src/application/repositories/notifications-repository';
+import { PrismaService } from './prisma/prisma.service';
+import { Module } from "@nestjs/common";
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  exports: [NotificationRepository]
+
+})
+export class DatabaseModule { }
---
+++ src/infra/http/controller/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { SendNotification } from './../../../application/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notifications-body';
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) { }
+  @Post()
+  async create (@Body() body: CreateNotificationBody) {
+    const { category, content, recipientId } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category
+    });
+
+    return { notification };
+  }
+}
