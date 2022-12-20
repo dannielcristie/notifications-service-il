dd510a2599b3b299dd03734d51a48e1abae51245
Danniel Cristie <dany14012@gmail.com>
2022-12-20 11:13:00 -0300

    test: add notification factory on test

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,8 +1,6 @@
 import { CountRecipientNotifications } from './count-recipient-notifications';
-import { Notification } from '@application/entities/notification';
-import { Content } from '../entities/content';
 import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
-import { CancelNotification } from './cancel-notification';
+import { makeNotification } from '@test/factories/notification-factory';
 
 describe('Count recipients notifications', () => {
 
@@ -11,28 +9,16 @@ describe('Count recipients notifications', () => {
     const countRecipientlNotifications = new CountRecipientNotifications(notificationsRepository);
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'Social',
-        content: new Content('this is a notification'),
-        recipientId: 'recipiendId-01',
-      })
-    )
+      makeNotification({ recipientId: 'recipiendId-01' })
+    );
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'Social',
-        content: new Content('this is a notification'),
-        recipientId: 'recipiendId-01',
-      })
-    )
+      makeNotification({ recipientId: 'recipiendId-01' })
+    );
 
     await notificationsRepository.create(
-      new Notification({
-        category: 'Social',
-        content: new Content('this is a notification'),
-        recipientId: 'recipiendId-02',
-      })
-    )
+      makeNotification({ recipientId: 'recipiendId-02' })
+    );
 
     const { count } = await countRecipientlNotifications.execute({
       recipientId: "recipiendId-01"
